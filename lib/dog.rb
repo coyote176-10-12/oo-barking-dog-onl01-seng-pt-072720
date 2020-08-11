e7e4da563e4f44545ac2115e8613429761c60f07
Javier Aguilar <coyote176@msn.com>
2020-08-11 02:18:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,15 @@
 class Dog 
   def name=(dogs_name)
     @doggie_name = dogs_name 
-  end
+ end
   def name 
     @doggie_name
-  end
-  def bark 
-    
-    @doggie_name
-    puts "woof!"
-   
 end
+  def bark 
+    @doggie_name
+      puts "woof!"
+   
+  end
   
 end
 
