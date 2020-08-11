e77533ded0145908683172226261c5247aab1f6a
Javier Aguilar <coyote176@msn.com>
2020-08-11 02:19:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+  def name=(dogs_name)
+    @doggie_name = dogs_name 
+ end
+  def name 
+    @doggie_name
+end
+  def bark 
+    @doggie_name
+      puts "woof!"
+   
+  end
+  
+end
+
+
+
